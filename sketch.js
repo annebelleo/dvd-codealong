376c386bef1e1735a4ed44edb837eb17d91fd4e0
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-07-25 20:19:08 +0000

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,18 +1,36 @@
-function preload() {
- 
+// variable defined with var, always define variable first
+// as opposed to ruby - you can skip this step in ruby
+var dvdImage
+var dvdX = 50
+var dvdY = 100
+var velocityX=5
+var velocityY=5
+var dvdWidth = 350
+var dvdHeight = 250
 
+// compared to ruby, you do not need to call in a variable in js
+function preload() {
+  // where you want to put stuff that automatically loads
+  dvdImage = loadImage("dvdlogo.jpg");
 }
 
 function setup() {
+  // size of screen
   createCanvas(windowWidth, windowHeight);
   background(100);
-
-
 }
 
 function draw() {
-
-
-
-
+  // 60fps
+  clear();
+  image(dvdImage, dvdX, dvdY, dvdWidth, dvdHeight);
+  dvdX = dvdX + velocityX
+  dvdY = dvdY + velocityY
+  if (dvdX > windowWidth - dvdWidth || dvdX == 0) {
+    velocityX = velocityX * -1
+  }
+  
+  if (dvdY > windowHeight - dvdHeight || dvdY==0) {
+    velocityY = velocityY * -1
+  }
 }
